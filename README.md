0e92b6354a31ddded86dee4e1042a732b5db12d3
Josh <joshlim092@gmail.com>
2022-02-21 23:21:05 +0000

    refactor withdraw and extracted transaction

---
+++ README.md
@@ -0,0 +1,30 @@
+Givena client makes a deposit of 1000 on 10-01-2023Anda deposit of 2000 on 13-01-2023Anda withdrawal of 500 on 14-01-2023
+Whenshe prints her bank statementThenshe would see
+
+account = Account.new
+account.deposit(amount: 500, date: 10-01-2023)
+account.deposit(amount: 2000, date: 13-01-2023)
+account.withdraw(amount: 500, date: 14-01-2023)
+account.show_statement
+
+Print:
+date || credit || debit || balance
+14/01/2023 || || 500.00 || 2500.00
+13/01/2023 || 2000.00 || || 3000.00
+10/01/2023 || 1000.00 || || 1000.00
+
+# Edge Cases
+
+Can't withdraw > balance amount, raise error if so
+
+require './lib/account.rb'
+
+a = Account.new
+a.deposit(500, '10-01-2023')
+a.withdraw(2000, '13-01-2023')
+a.balance
+a.withdraw(1000, '14-01-2023')
+
+a = Account.new
+t = Transaction.new
+t.deposit(500, '10-01-2023')
--- lib/account.rb
+++ lib/account.rb
@@ -13,22 +13,24 @@ class Account
     @transactions << {balance: @balance, credit: amount, date: date}
   end
 
+  # Extract Method 
   def withdraw(amount, date)
-    @balance -= amount
-    @transactions << {balance: @balance, debit: amount, date: date}
+    withdraw_balance = @balance -= amount
+    add_withdraw_transaction(amount, date) 
   end
 
+private
+
+  def add_withdraw_transaction(amount, date) 
+    transactions << {balance: @balance, debit: amount, date: date}
+  end 
+
+
   def show_statement
-    puts "date || credit || debit || balance"
+    "date || credit || debit || balance"
     @transactions.reverse.each do |transaction|
-      puts "#{transaction[:date]}|| #{transaction[:credit]}|| #{transaction[:debit]} || #{transaction[:balance]}"
+      "#{transaction[:date]}|| #{transaction[:credit]}|| #{transaction[:debit]} || #{transaction[:balance]}"
     end 
   end 
 end 
 
-
-a = Account.new
-a.deposit(500, '10-01-2023')
-a.withdraw(2000, '13-01-2023')
-a.balance
-a.withdraw(1000, '14-01-2023')
